2ac7be5a47547fb5885ccba8698bc12493e78dce
Grigor Danut <grigor@seamlesscare.ie>
2023-06-01 00:24:56 +0100

    Updated the file RegisterUser

--- app/src/main/java/com/example/danut/restaurant/RegisterUser.java
+++ app/src/main/java/com/example/danut/restaurant/RegisterUser.java
@@ -1,6 +1,5 @@
 package com.example.danut.restaurant;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -12,8 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
